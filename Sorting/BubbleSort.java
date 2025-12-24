5481bf65e1996e0422fdf9358e5543a5f0db734e
jagadeeshvadlamudi1818-source <jagadeeshvadlamudi1818@gmail.com>
2025-12-24 16:37:32 +0530

    Refactor Bubble Sort to correct sorting logic
    Refactored Bubble Sort implementation to use the correct algorithm.

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -9,15 +9,13 @@ public class BubbleSort {
         int arr[]={7,8,3,1,2};
 
         for (int i=0;i<=arr.length-1;i++) {
-            int smallest=i;
-         for(int j=i+1;j<arr.length;j++) {
-           if(arr[smallest]>arr[j]) {
-            smallest=j;
+         for(int j=0;j<arr.length-i-1;j++) {
+           if(arr[j]>arr[j+1]) {
+             int temp=arr[j];
+         arr[j]=arr[j+1];
+         arr[j+1]=temp; 
            }
-         } 
-         int temp=arr[smallest];
-         arr[smallest]=arr[i];
-         arr[i]=temp;  
+         }  
         }
        // printArray(arr);
        for(int i=0;i<arr.length;i++) {
