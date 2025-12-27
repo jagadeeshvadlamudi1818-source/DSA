1c2fc0e816e62977ceccb9e064539bc249b74950
Student ApnaCollege <your_email@gmail.com>
2025-12-27 20:26:38 +0530

    add folder in a file

---
+++ Array List/Hash Map/HashMap.java
@@ -0,0 +1,14 @@
+
+public class HashMap {
+    public static void main(String[] args) {
+
+        java.util.HashMap<String, Integer> hm = new java.util.HashMap<>();
+
+
+        hm.put("india", 100);
+        hm.put("china", 150);
+        hm.put("nepal", 50);
+
+        System.out.println(hm);
+    }
+}
