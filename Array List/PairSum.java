83f2d139904cbf0ee3f265d3f676849f89817296
Student ApnaCollege <your_email@gmail.com>
2025-12-27 09:56:43 +0530

    fix file into folder

---
+++ Array List/PairSum.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class PairSum {
+  public static boolean PairSum1(ArrayList<Integer>List,int target) {
+    for(int i = 0 ; i < List.size(); i++) {
+        for(int j = i+1; j < List.size(); j++) {
+            if(List.get(i)+List.get(j) == target) {
+                return true;
+            }
+        }
+    }
+    return false;
+  } 
+  public static void main(String[] args) {
+        ArrayList<Integer> List = new ArrayList<>();
+       List.add(1);
+       List.add(2);
+       List.add(3);
+       List.add(4);
+       List.add(5);
+       List.add(6);
+       int target=50;
+       System.out.println(PairSum1(List,target));
+    }      
+
+}
--- Array List/TwoPointer.java
+++ Array List/TwoPointer.java
@@ -1,33 +1,37 @@
+
 import java.util.*;
+
 public class TwoPointer {
+
     public static int StoreWater(ArrayList<Integer> height) {
-        int MaxWater=0;
-        int lp=0;
-        int rp=height.size()-1;
-        while(lp<rp) {
-            int ht=Math.min(height.get(lp),height.get(rp));
-            int width=rp-lp;
-            int CurrWater=ht*width;
-            MaxWater=Math.max(MaxWater,CurrWater);
-            if(height.get(lp)<height.get(rp)) {
-            lp++;
+        int MaxWater = 0;
+        int lp = 0;
+        int rp = height.size() - 1;
+        while (lp < rp) {
+            int ht = Math.min(height.get(lp), height.get(rp));
+            int width = rp - lp;
+            int CurrWater = ht * width;
+            MaxWater = Math.max(MaxWater, CurrWater);
+            if (height.get(lp) < height.get(rp)) {
+                lp++;
             } else {
                 rp--;
             }
         }
         return MaxWater;
     }
-        public static void main(String[] args) {
-        ArrayList<Integer> height=new ArrayList<>();
+
+    public static void main(String[] args) {
+        ArrayList<Integer> height = new ArrayList<>();
         height.add(1);
-         height.add(8);
-          height.add(6);
-           height.add(2);
-            height.add(5);
-             height.add(4);
-              height.add(8);
-               height.add(3);
-                height.add(7);
-                System.out.println(StoreWater(height));
+        height.add(8);
+        height.add(6);
+        height.add(2);
+        height.add(5);
+        height.add(4);
+        height.add(8);
+        height.add(3);
+        height.add(7);
+        System.out.println(StoreWater(height));
     }
 }
