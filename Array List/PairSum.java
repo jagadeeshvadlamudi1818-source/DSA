ae5bcf25d34a70da9811702386a0a9e7daa1e867
Student ApnaCollege <your_email@gmail.com>
2025-12-27 12:42:02 +0530

    fix file into folder

--- Array List/PairSum.java
+++ Array List/PairSum.java
@@ -1,15 +1,29 @@
 import java.util.*;
 public class PairSum {
-  public static boolean PairSum1(ArrayList<Integer>List,int target) {
-    for(int i = 0 ; i < List.size(); i++) {
-        for(int j = i+1; j < List.size(); j++) {
-            if(List.get(i)+List.get(j) == target) {
-                return true;
-            }
+ public static boolean PairSum1(ArrayList<Integer> List,int target) {
+    // for(int i = 0 ; i < List.size(); i++) {
+    //     for(int j = i+1; j < List.size(); j++) {
+    //         if(List.get(i)+List.get(j) == target) {
+    //             return true;
+    //         }
+    //     }
+    // }
+    // return false;
+    int lp=0;
+    int rp=List.size() - 1;
+    while(lp != rp) {
+
+        if(List.get(lp)+List.get(rp)==target) {
+            return true;
+        } if(List.get(lp)+List.get(rp)<target) {
+            lp++;
+        } else {
+            rp--;
         }
     }
     return false;
-  } 
+  }
+
   public static void main(String[] args) {
         ArrayList<Integer> List = new ArrayList<>();
        List.add(1);
---
+++ Array List/RotatePairSum.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class RotatePairSum {
+ public static boolean PairSum2(ArrayList<Integer> List,int target) {
+  int bp=0;
+  for(int i=0;i<List.size();i++) {
+    if(List.get(i)>List.get(i+1)) {
+        bp=i;
+        break;
+    }
+  }
+   int lp = bp + 1;
+   int rp=bp;
+   while(lp != bp) {
+    if(List.get(lp)+List.get(rp) == target) {
+        return true;
+    }
+    int n = List.size();
+    if(List.get(lp) + List.get(rp) < target) {
+        lp = (lp+1) % n;
+    } else {
+        rp = (lp+rp-1)%n;
+    }
+   }
+  return false;
+
+}
+
+   public static void main(String[] args) {
+        ArrayList<Integer> List = new ArrayList<>();
+       List.add(11);
+       List.add(15);
+       List.add(6);
+       List.add(8);
+       List.add(9);
+       List.add(10);
+       int target=16;
+       System.out.println(PairSum2(List,target));
+    }     
+}
